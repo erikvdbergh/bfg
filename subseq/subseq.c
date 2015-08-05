93360cfcc9b7d4b9c5801cd45b32a2a716feaa0c
erikvdbergh <erikvdbergh@posteo.de>
2015-08-05 10:21:39 +0100

    fast update

--- subseq/subseq.c
+++ subseq/subseq.c
@@ -32,21 +32,7 @@ void opts_init() {
   opts.filegiven = 0;
 }
 
-void parseopts2(int argc, char **argv) {
-  struct option longopts[] = {
-    {"file"  , required_argument, NULL, 'f'},
-    {"quiet"  , no_argument, NULL, 'q'},
-    {"silent"  , no_argument, NULL, 'q'},
-    {"no-messages", no_argument, NULL, 's'},
-    {"begin"  , required_argument, NULL, 'b'},
-    {"end"  , required_argument, NULL, 'e'}
-  };
-  char c;
-  while ((c = getopt_long(argc, argv, ":f:qsb:e:", longopts, NULL)) != -1) {
-  }
-}
-
-void parseopts(int argc, char **argv) {
+int parseopts(int argc, char **argv) {
   struct option longopts[] = {
     {"file"  , required_argument, NULL, 'f'},
     {"quiet"  , no_argument, NULL, 'q'},
@@ -78,31 +64,54 @@ void parseopts(int argc, char **argv) {
         opts.endgiven = 1;
         break;
       case ':':
-        fprintf(stderr, "%s: Option \'-%c\' requires an argument\n", argv[0], optopt);
+        if (!opts.quiet) {
+          fprintf(stderr, "%s: Option \'-%c\' requires an argument\n", argv[0], optopt);
+        }
+        return 1;
 	break;
       case '?':
       default:
-        fprintf(stderr, "%s: unrecognized option \'-%c\'", argv[0], optopt); 
+        if (!opts.quiet) {
+          fprintf(stderr, "%s: unrecognized option \'-%c\'", argv[0], optopt); 
+        }
+        return 1;
 	break;
     }
   }
 
   if (opts.begingiven && !opts.endgiven) {
     if (!opts.quiet) {
-      fprintf(stderr, "end coordinates must be specified (-e) if beginning coordinates are given\n");
+      fprintf(stderr, "subseq: End coordinates must be specified (-e) if beginning coordinates are given\n");
     }
+    return 1;
   }
 
   if (!opts.begingiven && opts.endgiven) {
     if (!opts.quiet) {
-      fprintf(stderr, "beginning coordinates must be specified (-b) if end coordinates are given\n");
+      fprintf(stderr, "subseq: beginning coordinates must be specified (-b) if end coordinates are given\n");
     }
+    return 1;
   }
 
   if (!opts.begingiven && !opts.endgiven) {
-    //opts.begin = atoi(argv[optind++]);
-    //opts.end = atoi(argv[optind++]);
-    //TODO needs error checking;
+    if (argv[optind]) {
+      opts.begin = atoi(argv[optind++]);
+      opts.end = atoi(argv[optind++]);
+    } else {
+      if (!opts.quiet) {
+        fprintf(stderr, "subseq: Begin and end coordinates need to be specified\n");
+      }
+      return 1;
+    }
+  }
+
+  if (opts.begin > opts.end) {
+    if (!opts.quiet) {
+      fprintf(stderr, "subseq: Begin coordinate is higher than end coordinate, swapping. Try revcomp for reverse complement\n";
+    }
+    int temp = opts.begin;
+    opts.begin = opts.end;
+    opts.end = temp;
   }
 
   if (!opts.filegiven) {
@@ -110,25 +119,94 @@ void parseopts(int argc, char **argv) {
       strcpy(opts.files[opts.file_i++], argv[optind++]);
     }
   }
+
+  return 0;
 }
 
-void process_file(FILE *fp) {
+int process_file(FILE *fp) {
+  int nuc_pos = 0;
+  int insub = 0;
+  char subseq[MAX_SEQ_LEN];
 
+  while (fscanf(fp, "%s", line) != EOF) {
+    if (line[0] == '>') {
+      nuc_pos = 0;
+    } else {
+      if (!insub) {
+        linelen = strlen(line);
+        if (nuc_pos + linelen > opts.begin && nuc_pos + linelen > opts.end) { // subseq in one line
+          if (line = substr(line, opts.begin - nuc_pos, opts.end - nuc_pos)) {
+            strcpy(subseq, line);
+          } else {
+            //substr error
+          }
+        } else if (nuc_pos + linelen > opts.begin) {
+          if (line = substr(line, opts.begin - nuc_pos, linelen)) {
+          strcpy(subseq, line);
+          } else {
+            //substr error
+          }
+          insub = 1;
+          continue;
+        }
+      } else {
+        linelen = strlen(line);
+        if (nuc_pos + linelen > opts.end) {
+          if (line = substr(line, nuc_pos, opts.end - nuc_pos)) {
+            strcpy(subseq, strcat(subseq, line);
+          } else {
+            // substr error
+          }
+        }
+      }
+    }
+  }
+}
+
+char *substr(char *str, int start, int end) {
+  if (start > end) {
+    int temp = start;
+    start = end;
+    end = temp;
+  }
+
+  int sl = strlen(str)
+  if (end - start > sl || end > sl || start > sl) {
+    fprintf(stderr, "substr: desired length longer than string\n";
+    return NULL;
+  }
+
+  int sublen = end - start;
+  char tempstr[sublen];
+  int temp_i;
+  for (temp_i = 0; temp_i < sublen; temp_i++) {
+    tempstr[temp_i] = str[start + temp_i];
+  }
+  tempstr[temp_i] = '\0';
+  strcpy(str, tempstr);
+
+  return str;
 }
 
 int main(int argc, char** argv) {
 
   opts_init();
-  parseopts(argc, argv);
+  if (parseopts(argc, argv)) { // something went wrong, returned 1
+    return 1;
+  }
 
-  if (opts.file_i == 0) {
-    process_file(stdin);
+  if (!opts.file_i) {
+    if (process_file(stdin)) {
+      return 1;
+    }
   }
 
   int file_i;
   for (file_i = 0; file_i < opts.file_i; file_i++) {
     FILE *fp = open_file(opts.files[file_i]);
-    process_file(fp);
+    if (process_file(fp)) {
+      return 1;
+    }
   }
 
   return 0;
