dbe3b99beccda5291abb0fdc459a9161a92d7e95
erikvdbergh <erikvdbergh@posteo.de>
2015-08-06 17:13:34 +0100

    cleanup, todo : subseq returns incorrect

--- subseq/subseq.c
+++ subseq/subseq.c
@@ -13,12 +13,14 @@
 #define MAX_SEQ_LEN 4294967296 // 4GB
 
 struct Opts {
-  char files[MAX_FILES][MAX_FILENAME_LEN]; //-f --file
+//  char files[MAX_FILES][MAX_FILENAME_LEN]; //-f --file
+  char **files;
   int file_i;
   int quiet; //-q --quiet --silent
   int nomsg; // -s --no-messages
   int begin; //-b --begin
   int end; // -e --end
+  int header_coord; // -h --header-coord add subseq coordinates to FASTA header line
 
   int begingiven;
   int endgiven;
@@ -28,6 +30,11 @@ struct Opts {
 struct Opts opts;
 
 void opts_init() {
+  opts.files = malloc(MAX_FILES * sizeof(char*));
+  int i;
+  for (i = 0; i < MAX_FILES ; i++) {
+    opts.files[i] = malloc(MAX_FILENAME_LEN*sizeof(char));
+  }
   opts.file_i = 0;
   opts.filegiven = 0;
 }
@@ -40,10 +47,11 @@ int parseopts(int argc, char **argv) {
     {"no-messages", no_argument, NULL, 's'},
     {"begin"  , required_argument, NULL, 'b'},
     {"end"  , required_argument, NULL, 'e'},
+    {"header-coord"  , required_argument, NULL, 'h'},
     {0, 0, 0, 0}
   };
   char c;
-  while ((c = getopt_long(argc, argv, ":f:qsb:e:", longopts, NULL)) != -1) {
+  while ((c = getopt_long(argc, argv, ":f:qsb:e:h", longopts, NULL)) != -1) {
     switch(c) {
       case 'f':
         strcpy(opts.files[opts.file_i++], optarg);
@@ -63,7 +71,9 @@ int parseopts(int argc, char **argv) {
         opts.end = atoi(optarg);
         opts.endgiven = 1;
         break;
-      case ':':
+       case 'h':
+        opts.header_coord = 1;
+       case ':':
         if (!opts.quiet) {
           fprintf(stderr, "%s: Option \'-%c\' requires an argument\n", argv[0], optopt);
         }
@@ -151,12 +161,21 @@ int substr(char *str, int start, int end) {
 int process_file(FILE *fp) {
   int nuc_pos = 0;
   int insub = 0;
-  char subseq[MAX_SEQ_LEN];
+  char subseq[2048];
+  char seqid[2048];
 
   char line[MAX_LINE_LEN];
 
   while (fscanf(fp, "%s", line) != EOF) {
     if (line[0] == '>') {
+      if (strcmp(seqid, "")) {
+        if (opts.header_coord) {
+          printf("%s|%i:%i\n%s\n", seqid, opts.begin, opts.end, subseq);
+        } else {
+          printf("%s\n%s\n", seqid, subseq);
+        }
+      }
+      strcpy(seqid, line);
       nuc_pos = 0;
     } else {
       int linelen = strlen(line);
@@ -178,6 +197,7 @@ int process_file(FILE *fp) {
           insub = 1;
           continue;
         }
+        insub = 1;
       } else {
         if (nuc_pos + linelen > opts.end) {
           if (!substr(line, nuc_pos, opts.end - nuc_pos)) {
@@ -213,10 +233,15 @@ int main(int argc, char** argv) {
 
   int file_i;
   for (file_i = 0; file_i < opts.file_i; file_i++) {
-    FILE *fp = open_file(opts.files[file_i]);
-    if (process_file(fp)) {
+    FILE *fp = fopen(opts.files[file_i], "r");
+    if (!fp) {
+      fprintf(stderr, "Can't open file %s\n", opts.files[file_i]);
+    }
+    int procret =  process_file(fp);
+    if (procret) {
       return 1;
     }
+
   }
 
   return 0;
